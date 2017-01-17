33a1a5258b4f774ce0b35df50e3110c87a3459c5
Mrinal bolar rao <mrinal.raob@gmail.com>
2017-01-17 09:28:54 +0530

    Adding New models and migration scripts

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,8 +1,15 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int CustomerId { get; set; }
+        [Required]
+        [StringLength(255)]
         public string CustomerName { get; set; }
+        public bool IsSubscribedTonewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
