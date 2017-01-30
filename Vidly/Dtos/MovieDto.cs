def5fcbe156352f792c5d13afc0760c7e1b2093b
Mrinal bolar rao <mrinal.raob@gmail.com>
2017-01-30 10:11:28 +0530

    Implementing Apis and automapper

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        [Required]
+        public byte GenereId { get; set; }
+    }
+}
